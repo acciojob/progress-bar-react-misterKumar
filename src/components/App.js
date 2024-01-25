7cd418ad1144ba18154b85be177bebc33f27d3c5
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-25 12:36:31 +0530

    check

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,39 @@
-
-import React from "react";
-import './../styles/App.css';
-
+import React, { useState, useEffect } from "react";
+import "./../styles/App.css";
+import 'regenerator-runtime/runtime'
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const [progress, setProgress] = useState(0);
 
-export default App
+  useEffect(() => {
+    // Asynchronous operation that updates the progress
+    const asyncOperation = async () => {
+      for (let i = 0; i < 100; i++) {
+        await new Promise((resolve) => setTimeout(resolve, 100));
+        setProgress(i + 1);
+      }
+    };
+
+    asyncOperation();
+  }, []);
+
+  const barInnerStyle = {
+    width: `${progress}%`,
+  };
+
+  return (
+    <div id="barOuter">
+      <div className="title" >
+        <h1>Progress Bar</h1>
+      </div>
+      <div id="barInner" style={barInnerStyle}>
+        
+      </div>
+      <br />
+      <div>
+      {`${progress}%`}
+      </div>
+    </div>
+  );
+};
+
+export default App;
