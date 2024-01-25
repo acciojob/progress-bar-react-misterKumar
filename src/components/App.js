36ec1f634e0ecb99baad4f2b9b5b695f607556f7
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-25 12:45:18 +0530

    check

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,45 @@
-import React, { useState, useEffect } from "react";
-import "./../styles/App.css";
-import 'regenerator-runtime/runtime'
-const App = () => {
-  const [progress, setProgress] = useState(0);
-
-  useEffect(() => {
-    // Asynchronous operation that updates the progress
-    const asyncOperation = async () => {
-      for (let i = 0; i < 100; i++) {
-        await new Promise((resolve) => setTimeout(resolve, 100));
-        setProgress(i + 1);
+                                                    
+import React,{useState,useEffect} from "react";  
+import 'regenerator-runtime/runtime';      
+import './../styles/App.css';              
+import Progressbar from "./Progressbar";           
+const App = () => {                                                            
+  const [progress, setProgress] = useState(0);              
+ const percent =10;                                 
+  useEffect(() => {                               
+                                                  
+    const asyncOperation = () => {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          resolve();
+        }, 1000);
+      });
+    };
+                                                           
+    const updateProgress = async () => {
+      // update the progress bar value every second until it reaches 100%
+      for (let i = 10; i <= 100; i+=10) {
+        await asyncOperation();
+        setTimeout(() => {
+          setProgress(i);
+        }, 1000);
+        setProgress(i);
       }
     };
 
-    asyncOperation();
-  }, []);
-
-  const barInnerStyle = {
-    width: `${progress}%`,
-  };
-
-  return (
-    <div id="barOuter">
-      <div className="title" >
-        <h1>Progress Bar</h1>
-      </div>
-      <div id="barInner" style={barInnerStyle}>
-        
-      </div>
-      <br />
-      <div>
-      {`${progress}%`}
-      </div>
-    </div>
+    updateProgress();
+  }, []);                   
+                                                        
+  return (                                       
+    <div>
+        {/* Do not remove the main div */}
+      
+       <h1>My Progress Bar</h1>
+    <Progressbar progress={progress} />
+        <div id="barOuter,barInner">{progress}%</div>
+        <hr/>
+    </div>                                                                                              
   );
-};
+}
 
 export default App;
